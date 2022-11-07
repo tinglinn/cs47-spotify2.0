e3ee3ee6fda459ea78a262805a6524a2b036483c
ting <tinglin0708@gmail.com>
2022-11-06 21:52:38 -0800

    added stack navigator

--- App.js
+++ App.js
@@ -1,25 +1,43 @@
+import React from 'react';
 import { StyleSheet, SafeAreaView, Text, FlatList, Pressable, View, Image } from "react-native";
 import { useSpotifyAuth, millisToMinutesAndSeconds } from "./utils";
 import { Images, Themes } from "./assets/Themes";
-import { ScrollView } from "react-native-web";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { WebView } from 'react-native-webview';
+
+const Stack = createStackNavigator()
 
 export default function App() {
   // Pass in true to useSpotifyAuth to use the album ID (in env.js) instead of top tracks
-  const { token, tracks, getSpotifyAuth } = useSpotifyAuth();
-
-  let contentDisplayed = null;
+  const { token, tracks, getSpotifyAuth } = useSpotifyAuth()
   
-  if (token) {
-    contentDisplayed = <SongList tracks={tracks} />
-  } else {
-    contentDisplayed = <ConnectButton getSpotifyAuth={getSpotifyAuth} />
+  let contentDisplayed = null;
+
+  function HomeScreen({navigation}) {
+    if (token) {
+      contentDisplayed = <Playlist navigation={navigation}  tracks={tracks} />
+    } else {
+      contentDisplayed = <ConnectButton getSpotifyAuth={getSpotifyAuth} />
+    }
+
+    return (
+      <SafeAreaView style={styles.container}>
+        {contentDisplayed}
+      </SafeAreaView>
+    )
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      {contentDisplayed}
-    </SafeAreaView>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="SongDetail" component={SongDetailScreen} />
+        <Stack.Screen name="SongPreview" component={SongPreviewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const ConnectButton = ({ getSpotifyAuth }) => {
@@ -28,57 +46,72 @@ const ConnectButton = ({ getSpotifyAuth }) => {
       <Pressable onPress={getSpotifyAuth}>
         <View style={styles.buttonContainer}>
           <Image style={styles.spotifyIcon} source={Images.spotify} />
-          <Text style={{color: 'white', fontFamily: "Helvetica", fontWeight: 'bold'}}>CONNECT WITH SPOTIFY</Text>
+          <Text style={{ color: 'white', fontFamily: "Helvetica", fontWeight: 'bold' }}>CONNECT WITH SPOTIFY</Text>
         </View>
       </Pressable>
     </View>
   );
 }
 
-const Header = () => {
+const Playlist = ({ navigation, tracks }) => {
   return (
-    <View style={styles.header}>
-      <Image style={styles.spotifyIconHeader} source={Images.spotify} />
-      <Text style={{ color: 'white', fontSize: 22, fontFamily: "Helvetica", fontWeight: 'bold' }}>My Top Tracks</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.spotifyIconHeader} source={Images.spotify} />
+        <Text style={{ color: 'white', fontSize: 22, fontFamily: "Helvetica", fontWeight: 'bold' }}>My Top Tracks</Text>
+      </View>
+      <FlatList scroll={true} data={tracks} renderItem={({ item }) => RenderTrack(item, navigation)} keyExtractor={(item) => item.id} />
+    </SafeAreaView>
   )
 }
 
-const SongList = ({tracks}) => {
+// renderItem={({ index, item }) => RenderTrack(navigation, index, item)}
+const RenderTrack = (item, navigation) => {  // renders one song
+  const image_url = item.album.images[0].url;
+  const artist_name = item.album.artists[0].name;
+  const song_name = item.name;
+  const album_name = item.album.name;
+  const duration = millisToMinutesAndSeconds(item.duration_ms);
+  const song_external_url = item.external_urls.spotify;
+  const song_preview_url = item.preview_url;
+
   return (
-    <View>
-      <Header />
-      <FlatList scroll={true} data={tracks} renderItem={RenderTrack} keyExtractor={(item) => item.id} />
-    </View>
+    <Pressable onPress={() => navigation.navigate("SongDetail", { external_url: song_external_url })}>
+      <View style={styles.songContainer}>
+        <View style={{ width: "10%", paddingLeft: 5 }}>
+          <Pressable onPress={(e) => { e.stopPropagation(); navigation.navigate("SongPreview", { preview_url: song_preview_url }) }}>
+            <Ionicons name="md-play-circle" size={20} color={Themes.colors.spotify} />
+          </Pressable>
+        </View>
+
+        <Image style={styles.songCover} source={{ uri: image_url }} />
+        <View style={{ width: "35%", marginLeft: 10, marginRight: 10, flexDirection: "column", justifyContent: "center", alignItems: "flex-start" }}>
+          <Text numberOfLines={1} style={styles.songText}>{song_name}</Text>
+          <Text numberOfLines={1} style={styles.grayText}>{artist_name}</Text>
+        </View>
+
+        <View style={{ width: "22%" }}>
+          <Text numberOfLines={1} style={styles.songText}>{album_name}</Text>
+        </View>
+        <View style={{ width: "10%", paddingRight: 5 }}>
+          <Text style={styles.songText}>{duration}</Text>
+        </View>
+      </View>
+    </Pressable>
   )
 }
 
-const RenderTrack = ({ index, item }) => {  // renders one song
-  const image_url = item.album.images[0].url
-  const artist_name = item.album.artists[0].name
-  const song_name = item.name
-  const album_name = item.album.name
-  const duration = millisToMinutesAndSeconds(item.duration_ms)
-  
+const SongDetailScreen = ({ navigation, route }) => {
+  const { external_url } = route.params;
   return (
-    <View style={styles.songContainer}>
-      <View style={{ width: "10%", paddingLeft: 5 }}> 
-        <Text style={styles.grayText}>{index + 1}</Text>
-      </View>
-      
-      <Image style={styles.songCover} source={{ uri: image_url }} />
-      <View style={{width: "35%", marginLeft: 10, marginRight: 10, flexDirection: "column", justifyContent: "center", alignItems: "flex-start"}}>
-        <Text numberOfLines={1} style={styles.songText}>{song_name}</Text>
-        <Text numberOfLines={1} style={styles.grayText}>{artist_name}</Text>
-      </View>
+    <WebView source={{ uri: external_url }} />
+  )
+}
 
-      <View style={{width: "22%"}}>
-        <Text numberOfLines={1} style={styles.songText}>{album_name}</Text>
-      </View>
-      <View style={{width: "10%", paddingRight: 5}}>
-        <Text style={styles.songText}>{duration}</Text>
-      </View>
-    </View>
+const SongPreviewScreen = ({ navigation, route }) => {
+  const { preview_url } = route.params;
+  return (
+    <WebView source={{ uri: preview_url }} />
   )
 }
 
